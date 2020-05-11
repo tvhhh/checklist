fc2adc5fbdc2032e1072c0e9707e99538e74db33
theciderizer <nthieu2501@gmail.com>
2020-05-11 21:57:54 +0700

    calendarUpdate
    calendarView

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,7 +4,19 @@ import styles from './styles';
 import EvilIcons from 'react-native-vector-icons/EvilIcons';
 
 export default class Search extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      search: '',
+    };
+  }
+
+  updateState = search => {
+    this.setState({search})
+  };
+
   render() {
+    const { search } = this.state
     return (
       <SearchBar
         round
@@ -12,6 +24,8 @@ export default class Search extends React.Component {
         inputContainerStyle={styles.inputContainer}
         inputStyle={{ fontSize: this.props.fontSize || 18 }}
         placeholder={this.props.placeholder}
+        onChangeText={this.updateState}
+        value={search}
         searchIcon={() => (<EvilIcons name="search" size={this.props.size || 25} />)}
       />
     );
