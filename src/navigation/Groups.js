0ed52f302c7a351e04489f6fb3ba4daa620631ff
Khoa <khoa.lu123@hcmut.edu.vn>
2020-06-12 20:21:41 +0700

    Change data from hardcode in UI to hardcode in app

--- src/navigation/Groups.js
+++ src/navigation/Groups.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Dimensions, ScrollView, View, Text, EventSubscriptionVendor, TouchableOpacity, SectionList, FlatList, StyleSheet } from 'react-native';
-import { Overlay } from 'react-native-elements';
+import { Alert, Dimensions, ScrollView, View, Text, EventSubscriptionVendor, TouchableOpacity, SectionList, FlatList, StyleSheet } from 'react-native';
 import NoticeBox from '../components/Notification/index';
 import Button from '../components/Button/index';
 import colors from '../styles/colors';
 import Header from '../components/Header/index';
 import Octicons from 'react-native-vector-icons/Octicons';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import AntDesgin from 'react-native-vector-icons/AntDesign'
 
 import {StackNavigator} from 'react-navigation'
 import { createStackNavigator } from '@react-navigation/stack';
 import { switchCase } from '@babel/types';
+import { white } from 'color-name';
+// import console = require('console');
+// import console = require('console');
 
 // import Group from '../components/GroupList/index';
 
@@ -26,12 +29,152 @@ const TASK_STATES = {
   NOT_RELATED: 'not-related',
 }
 
+const RETURN_STATUS = {
+  OK: 'ok',
+  UID_NOT_FOUND: 'uid-not-found',
+};
+
+const TEST_DATA = {
+  users: [
+    {
+      id: 0,
+      name: 'khoa',
+      groups: [
+        {
+          id: 0, 
+          policy: POLICIES.OWNER,
+        }
+      ],
+
+      invitations: [
+        {
+          id: 0,
+          name: "another group",
+        }
+      ],
+
+    }
+  ],
+
+  groups: [
+    {
+      id: 0,
+      name: 'It has a name',
+      users: [
+        {
+          id: 0,
+          name: 'khoa',
+          policy: POLICIES.OWNER,
+        }
+      ],
+      tasks: [
+        {
+          name: '123',
+          description: '123456',
+          dueDate: '22/22/2022',
+          participants: [
+            {
+              id: 0,
+              done: false,
+            }
+          ],
+        }
+      ],
+    },
+
+    {
+      id: 1,
+      name: "another group",
+      users: [],
+      tasks: [],
+    }
+  ],
+}
+
+/* class Server: is supposed to be a running server
+
+*/
+
+class Server {
+  constructor(data) {
+    this.users = data.users; // infomation of all users, users: [ {id, username, groups: [{gid, policy}]} ]
+    this.groups = data.groups; // infomation of all groups, groups:[ {id, title, tasks: {name, description, dueDateTime, participants, state}, users: [{uid, policy}] } ]
+  }
+
+
+  /*
+  input: 
+    uid: int
+      user's id
+
+
+  output: {status, (userInfo), (groupsInfo), (invitationsInfo) }
+    status: RETURN_STATUS
+      -- return status (been list in constructor)
+
+    userInfo: User, return when status = OK
+      -- infomation about user
+
+    groupsInfo: {id, policy, users, tasks, pendingUsers}, return when status = OK
+      id: int
+        -- id of group
+      users: {id, name, policy}[]
+        -- array containing info about all users of this group
+        id: int
+          -- id of user
+        name: string
+          -- name of user
+        policy: POLICIES
+          -- user with id policy in this group
+      tasks: GroupTask[]
+        -- array containing all info of taks of this group
+      pendingUsers: {id, name}[]
+        -- array contatining all info of pending users of this group
+        id: int
+          -- id of user
+        name: string
+          -- name of user
+    
+    invitationsInfo: {groupId, groupName}, return when status = OK
+      groupId: int
+        -- group id
+      groupName: string
+        -- group name
+  */
+  getClientInfo(uid) {
+    let res = {};
+    let userInfo = this.users.filter(user => {return user.id === uid});
+    if (userInfo.length !== 0) {
+      res.userInfo = {name: userInfo[0].name, groups: userInfo[0].groups};
+      res.invitationsInfo = userInfo[0].invitations;
+
+      res.groupsInfo = [];
+      for(const group of this.groups) {
+        for (const user of group.users) {
+          if (user.id === uid) {
+            res.groupsInfo.push(group);
+          }
+        }
+      }
+      res.status = RETURN_STATUS.OK;  
+    }
+    else {
+      res.status = RETURN_STATUS.UID_NOT_FOUND;
+    }
+    return res;
+  }
+}
+
+/* class User: contains information about a user
+  
+*/
+
 class User {
-  static UID = 0;
-  constructor(name) {
+  constructor(uid, name) {
+    this.uid = uid;
     this.name = name;
     this.groups = []; // id and user policy
-    this.tasks = []; // task
+    this.invitations = []; // groups id
   }
 
   addGroup(groupId, policy) {
@@ -41,11 +184,11 @@ class User {
   removeGroup(groupId) {
     this.groups = this.groups.filter(item=>item.groupId != groupId);
   }
-
-  addTask(task, groupId) {
-    // send request to server ? 
-  }
 }
+
+/* class GroupTask: contains information about a task.
+  
+*/
 
 class GroupTask {
   constructor(name, description, dueTime, participants) {
@@ -56,42 +199,67 @@ class GroupTask {
   }
 }
 
+/* class Group: contains information about a group.
+
+*/
+
 class Group {
-  constructor(name) {
+  constructor(id, name) {
+    this.id = id;
     this.name = name; // string
-    this.users = []; // id and policy
+    this.users = []; // {id: int, policy: POLICIES} []
     this.pendingUsers = []; // id
+    this.tasks = []; // GroupTask[]
   }
 }
 
-const DATA = [
-  {
-    title: "Groups you manage",
-    data: [
-      "group1", "group2", "group3", 
-    ],
-  },
+/*class Client: the class which is emmbeded to the app, back end (?)*/
 
-  {
-    title: "Groups you're in",
-    data: [
-      "group1", "group2", "group3", "group4", 
-    ],
-  },
+class Client {
+  constructor(server) {
+    this.uid = -1; // user id
+    this.server = server; // (fake) server connection 
+    this.clientInfo = null;
+  }
 
-  {
-    title: "Invitations",
-    data: [
-      "group1", "group2",
-    ],
-  },
-]
+  setUid(uid) {
+    this.uid = uid;
+  }
+
+  getAllInfo() {
+    this.clientInfo = {...this.server.getClientInfo(this.uid)}; // deepcopy, to simulate getting data from server.
+    if (this.clientInfo.status == RETURN_STATUS.UID_NOT_FOUND) {
+      console.debug("Client, getAllInfo(), UID = " + this.uid + ": NOT FOUND ON SERVER !");
+    } else
+    {
+      // adding policy to groupsInfo.groups.group
+      for(let group of this.clientInfo.groupsInfo) {
+        for(let userGroup of this.clientInfo.userInfo.groups) {
+          if (group.id === userGroup.id) {
+            group.policy = userGroup.policy;
+          }
+        }
+      }
+      console.debug("UID = " + this.uid + " FOUND ON SERVER !");
+      console.debug(this.clientInfo);
+    }
+  }
+}
+
+let server = new Server(TEST_DATA);
+let client = new Client(server);
+client.setUid(0);
+client.getAllInfo();
+
+/*BACK END STUFF END HERE*/
 
 class DropDown extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       menuText: this.props.menuText,
+      groups: this.props.groups,
+      type: this.props.type,
       menu: [
         {
           key: 'It has a name now',
@@ -138,20 +306,6 @@ class DropDown extends React.Component {
 
           ],
         },
-        {key: 'group2'},
-        {key: 'group3'},
-        {key: 'group4'},
-        {key: 'group5'},
-        {key: 'group6'},
-        {key: 'group7'},
-        {key: 'group8'},
-        {key: 'group9'},
-        {key: 'group10'},
-        {key: 'group11'},
-        {key: 'group12'},
-        {key: 'group13'},
-        {key: 'group14'},
-
       ],
 
       isShowingMenu: false,
@@ -183,24 +337,31 @@ class DropDown extends React.Component {
         <View>
           <FlatList
             styles={{marginTop:5}}
-            data={this.state.menu}
+            data={this.state.groups}
             renderItem = {({item}) => 
               <TouchableOpacity
                 onPress={()=>{
-                  this.props.navigation.navigate('Group', {
-                    title: item.key,
-                    tasks: item.tasks,
-                  });
+                  if (this.state.type === "manage" || this.state.type === "member")
+                  {
+                    this.props.navigation.navigate('group', {
+                      title: item.name,
+                      group: item,
+                    });
+                  }
+                  else 
+                  {
+                    this.props.navigation.navigate('invitation'); // should add something here...
+                  }
                 }}
               >
-                <Text style={styles.itemText}> {item.key} </Text>
+                <Text style={styles.itemText}> {item.name} </Text>
               </TouchableOpacity>
               
             }
 
           />
         </View>:
-        <View></View>
+        null
         }
         
       </View>
@@ -209,32 +370,74 @@ class DropDown extends React.Component {
 }
 
 function HomeView({navigation}) {
+  const DATA = [
+    {
+      menuText: "Groups you manage",
+      type: "manage",
+      groups: client.clientInfo.groupsInfo.filter(group => {return group.policy === POLICIES.OWNER || group.policy === POLICIES.ADMIN}),
+    },
+
+    {
+      menuText: "Groups you're in",
+      type: "member",
+      groups: client.clientInfo.groupsInfo.filter(group => {return group.policy === POLICIES.MEMBER}),
+    },
+
+    {
+      menuText: "Invitations",
+      type: "invitation",
+      groups: client.clientInfo.invitationsInfo,
+    },
+  ];
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => 
+        <TouchableOpacity
+          // onPress={() => navigation.navigate('info', {
+          //   groupName: route.params.title,
+          //   title: "Info: " + route.params.title,
+          // })}
+        >
+          <AntDesgin
+            name="addusergroup"
+            size={25}
+          />
+        </TouchableOpacity>,
+
+      headerRightContainerStyle: {padding: 15}
+    
+    });
+  }, [navigation]);
+
   return (
     <View>
-      <SectionList 
-        sections={DATA}
-        keyExtractor={(item, index) => item+index}
-        renderItem={({item})=> <View />}
-        renderSectionHeader={({section: {title}})=> (
-          <DropDown 
-            menuText={title}
-            navigation={navigation}  
+      <FlatList
+        data={DATA}
+        // keyExtractor={(item) => item}
+        renderItem={({item}) => 
+          <DropDown
+            menuText={item.menuText}
+            type={item.type}
+            groups={item.groups}
+            navigation={navigation}
           />
-        )}
+        }
       />
     </View>
   );
 }
 
 function GroupView({route, navigation}) {
-  const { tasks } = route.params;
+  const { group } = route.params;
+  const tasks = group.tasks;
 
   React.useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => 
         <TouchableOpacity
           onPress={() => navigation.navigate('info', {
-            groupName: route.params.title,
+            group: group,
             title: "Info: " + route.params.title,
           })}
         >
@@ -252,6 +455,7 @@ function GroupView({route, navigation}) {
   
   return (
     <View>
+      <View>
       <FlatList
         data={tasks}
         renderItem={({ item }) => 
@@ -269,6 +473,7 @@ function GroupView({route, navigation}) {
             <Text style={{fontWeight:'bold', fontSize:16, flex:1}}> {item.name} </Text>
             <FontAwesome5 
               name = "circle"
+              // fix this
               color={item.state==TASK_STATES.NOT_RELATED?"#acacac":item.state==TASK_STATES.NOT_DONE?"#fdd023":item.state==TASK_STATES.DONE?"#009900":"#000000"}
               style={{margin:10}}
               size={20}
@@ -298,8 +503,12 @@ function GroupView({route, navigation}) {
           </TouchableOpacity>
         }
       />
+      </View>
 
-      <Button.Create />
+      <View>
+        <Button.Create />
+
+      </View>
 
     </View>
   );
@@ -307,7 +516,7 @@ function GroupView({route, navigation}) {
 
 function InfoView({route, navigation}) {
 
-  const { groupName } = route.params;
+  const { group } = route.params;
   const localStyles = {
     container: {
       backgroundColor: '#1e90ff',
@@ -321,27 +530,39 @@ function InfoView({route, navigation}) {
       color:'#ffffff', 
       fontFamily:'sans-serif-light', 
       fontSize: 20,
+      marginLeft:8,
     }
   }
 
+  function createLeaveConfirmOverlay() {
+    Alert.alert(
+      "Confirm",
+      "Leaving this group ?",
+      [
+        {
+          text: "Cancel",
+          // onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        { 
+          text: "OK", 
+          onPress: () => {
+
+            navigation.popToTop();
+          }
+        }
+      ],
+      { cancelable: false }
+    );
+
+  }
+  
   return (
     <View>
       <TouchableOpacity
         onPress={() => navigation.navigate('members', {
-          title: 'Members: ' + groupName,
-          members: [
-            {name:'khoa', policy:POLICIES.OWNER},
-            {name:'hung', policy:POLICIES.ADMIN},
-            {name:'huy', policy:POLICIES.ADMIN},
-            {name:'huan', policy:POLICIES.ADMIN},
-            {name:'hoang', policy:POLICIES.ADMIN},
-            {name:'hieu', policy:POLICIES.ADMIN},
-            {name:'a ghost ?', policy:POLICIES.MEMBER},
-            {name:'red', policy:POLICIES.MEMBER},
-            {name:'green', policy:POLICIES.MEMBER},
-            {name:'blue', policy:POLICIES.MEMBER},
-            {name:'white', policy:POLICIES.MEMBER},
-          ],
+          title: 'Members: ' + group.name,
+          members: group.users,
         })}
         style={localStyles.container}
       > 
@@ -357,10 +578,10 @@ function InfoView({route, navigation}) {
 
       <TouchableOpacity
         style={localStyles.container}
+        onPress={createLeaveConfirmOverlay}
       > 
         <Text style={localStyles.item}> {"Leave group"} </Text>  
       </TouchableOpacity>
-
     </View>
   );
 }
@@ -422,11 +643,93 @@ function MemberView({route, navigation}) {
   );
 }
 
+function InvitationView({route, navigation}) {
+  const localStyles = {
+    infoContainer: {
+      backgroundColor: '#ffffff',
+      padding: 12,
+      marginVertical: 5,
+      marginHorizontal: 8,
+      
+      borderRadius: 5,
+    },
+
+    infoText: {
+      color:'#000000', 
+      fontFamily:'sans-serif-light', 
+      fontSize: 24,
+      marginLeft:8,
+    },
+
+    acceptContainter: {
+      backgroundColor: '#00ff00',
+      flex:1,
+      padding: 12,
+      marginVertical: 5,
+      marginHorizontal: 8,
+      alignItems:'center',
+      
+      borderRadius: 5,
+    },
+
+    denyContainer: {
+      backgroundColor: '#ff0000',
+      flex:1,
+      padding: 12,
+      marginVertical: 5,
+      marginHorizontal: 8,
+      alignItems:'center',
+      
+      borderRadius: 5,
+    },
+
+    confirmText: {
+      color: '#ffffff',
+      fontSize: 20,
+    },
+  };
+
+  return(
+    <View>
+      <View
+        style={localStyles.infoContainer}
+      >
+        <Text
+          style={localStyles.infoText}
+        >
+          Group name goes here
+        </Text>
+      </View>
+
+      <View style={{flexDirection:'row'}}>
+        <TouchableOpacity
+          style={localStyles.denyContainer}
+          onPress={() => {navigation.popToTop();}}
+        >
+          <Text style={localStyles.confirmText}>Deny</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={localStyles.acceptContainter}
+          onPress={() => {navigation.popToTop();}}
+        >
+          <Text style={localStyles.confirmText}>Accept</Text>
+        </TouchableOpacity>
+
+        
+      </View>
+    </View>
+  );
+}
+
 const Stack = createStackNavigator();
 ``
 export default class Groups extends React.Component {
   constructor (props) {
     super(props);
+    this.state = {
+      client: client,
+    }
   }
 
   toggleDrawer = () => {
@@ -442,17 +745,17 @@ export default class Groups extends React.Component {
 
         <Stack.Navigator>
           <Stack.Screen 
-            name="Home" 
+            name="home" 
             component={HomeView} 
             options={{title:'Home'}}  
           />
 
           <Stack.Screen 
-            name="Group" 
+            name="group" 
             component={GroupView} 
             options={({navigation, route}) => ({
               headerTitle: route.params.title,              
-              })}
+            })}
           />
 
           <Stack.Screen 
@@ -472,6 +775,14 @@ export default class Groups extends React.Component {
             })}
           />
 
+          <Stack.Screen
+            name="invitation"
+            component={InvitationView}
+            options={({route}) => ({
+              title: 'Invitations'
+            })}
+          />
+
         </Stack.Navigator>
       </View>
     );
@@ -481,13 +792,12 @@ export default class Groups extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 5,
     backgroundColor: '#1e90ff',
     flexDirection: "row",
     alignItems: "center",
-    marginTop: 16,
-    marginLeft: 8,
-    marginRight: 8,
+    padding: 6,
+    marginVertical: 5,
+    marginHorizontal: 8,
     
     borderRadius: 5,
   },
@@ -498,7 +808,7 @@ const styles = StyleSheet.create({
     fontFamily: "sans-serif-light",
     fontSize: 20,
     paddingVertical: 8,
-    paddingLeft: 20,
+    marginLeft:8,
     flex: 1,
   },
 
