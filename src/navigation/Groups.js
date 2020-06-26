204b299bd2e7e2572908c48d8a2ee836f45b942b
Khoa <khoa.lu123@hcmut.edu.vn>
2020-06-26 13:14:17 +0700

    add more group data to internal data json object

--- src/navigation/Groups.js
+++ src/navigation/Groups.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import { Alert, Dimensions, ScrollView, View, Text, EventSubscriptionVendor, TouchableOpacity, SectionList, FlatList, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, Dimensions, ScrollView, View, Text, EventSubscriptionVendor, TouchableOpacity, SectionList, FlatList, StyleSheet, TextInput } from 'react-native';
 import NoticeBox from '../components/Notification/index';
 import Button from '../components/Button/index';
 import colors from '../styles/colors';
 import Header from '../components/Header/index';
 import Octicons from 'react-native-vector-icons/Octicons';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-import AntDesgin from 'react-native-vector-icons/AntDesign'
+import AntDesgin from 'react-native-vector-icons/AntDesign';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+import TaskForm from '../components/Form/TaskForm';
 
 import {StackNavigator} from 'react-navigation'
 import { createStackNavigator } from '@react-navigation/stack';
@@ -43,13 +46,19 @@ const TEST_DATA = {
         {
           id: 0, 
           policy: POLICIES.OWNER,
+        },
+        {
+          id: 2,
+          policy: POLICIES.MEMBER,
         }
       ],
 
       invitations: [
         {
           id: 0,
-          name: "another group",
+          name: 'another group',
+          uid: 1,
+          uName: 'not khoa'
         }
       ],
 
@@ -84,9 +93,35 @@ const TEST_DATA = {
 
     {
       id: 1,
-      name: "another group",
+      name: 'another group',
       users: [],
       tasks: [],
+    },
+
+    { 
+      id: 2,
+      name: 'Starving',
+      users: [
+        {
+          id: 0,
+          name: 'khoa',
+          policy: POLICIES.MEMBER,
+        }
+      ],
+
+      tasks: [
+        {
+          name: 'Alo alo',
+          description: '123456',
+          dueDate: '01/012022',
+          participants: [
+            {
+              id: 0,
+              done: false,
+            }
+          ],
+        }
+      ],
     }
   ],
 }
@@ -135,7 +170,7 @@ class Server {
         name: string
           -- name of user
     
-    invitationsInfo: {groupId, groupName}, return when status = OK
+    invitationsInfo: {gid, gName, uid, uName}, return when status = OK
       groupId: int
         -- group id
       groupName: string
@@ -240,6 +275,7 @@ class Client {
           }
         }
       }
+
       console.debug("UID = " + this.uid + " FOUND ON SERVER !");
       console.debug(this.clientInfo);
     }
@@ -350,7 +386,9 @@ class DropDown extends React.Component {
                   }
                   else 
                   {
-                    this.props.navigation.navigate('invitation'); // should add something here...
+                    this.props.navigation.navigate('invitation', {
+                      group: item,
+                    }); // should add something here...
                   }
                 }}
               >
@@ -394,6 +432,7 @@ function HomeView({navigation}) {
     navigation.setOptions({
       headerRight: () => 
         <TouchableOpacity
+          onPress={() => {navigation.navigate('create-group')}}
           // onPress={() => navigation.navigate('info', {
           //   groupName: route.params.title,
           //   title: "Info: " + route.params.title,
@@ -414,7 +453,7 @@ function HomeView({navigation}) {
     <View>
       <FlatList
         data={DATA}
-        // keyExtractor={(item) => item}
+        keyExtractor={item => item.type}
         renderItem={({item}) => 
           <DropDown
             menuText={item.menuText}
@@ -432,9 +471,35 @@ function GroupView({route, navigation}) {
   const { group } = route.params;
   const tasks = group.tasks;
 
+  const maxLengthForTitle = 15;
+  const title  = route.params.title.length >  maxLengthForTitle ?
+                  route.params.title.substring(0, maxLengthForTitle-3) + '...':
+                  route.params.title;
+
   React.useLayoutEffect(() => {
     navigation.setOptions({
+
+      headerTitle: title,
+
       headerRight: () => 
+      <View style={{flexDirection:'row'}}>
+
+        {/*add button*/}
+        {
+        (group.policy === POLICIES.OWNER || group.policy == POLICIES.ADMIN) &&
+        <TouchableOpacity
+          style={{marginRight:20}}
+        >
+          <MaterialIcons
+            name={"add-circle-outline"}
+            size={25}
+          />
+
+          
+        </TouchableOpacity>
+        }
+
+        {/*info button*/}
         <TouchableOpacity
           onPress={() => navigation.navigate('info', {
             group: group,
@@ -445,7 +510,10 @@ function GroupView({route, navigation}) {
             name="info"
             size={25}
           />
-        </TouchableOpacity>,
+        </TouchableOpacity>
+
+        
+      </View>,
 
       headerRightContainerStyle: {padding: 15}
     
@@ -454,10 +522,10 @@ function GroupView({route, navigation}) {
 
   
   return (
-    <View>
-      <View>
+    <View style={{justifyContent:'flex-end'}}>
       <FlatList
         data={tasks}
+        keyExtractor={item => item.id}
         renderItem={({ item }) => 
           <TouchableOpacity
             style={{
@@ -497,18 +565,19 @@ function GroupView({route, navigation}) {
               style={{margin:10}}
               size={10}
             />
-            <Text> {item.participants.toString()} </Text>
+            <Text> {item.participants.map(user => {
+              for(const userInGroup of group.users) {
+                if (userInGroup.id === user.id)
+                {
+                  return userInGroup.name;
+                }
+              }
+            }).toString()} </Text>
           </View>
 
           </TouchableOpacity>
         }
       />
-      </View>
-
-      <View>
-        <Button.Create />
-
-      </View>
 
     </View>
   );
@@ -570,11 +639,14 @@ function InfoView({route, navigation}) {
         <Text style={localStyles.item}> {"Members"} </Text>  
       </TouchableOpacity>
 
+      {
+      (group.policy === POLICIES.OWNER || group.policy == POLICIES.ADMIN) &&
       <TouchableOpacity
         style={localStyles.container}
       > 
         <Text style={localStyles.item}> {"Add more people"} </Text>  
       </TouchableOpacity>
+      }
 
       <TouchableOpacity
         style={localStyles.container}
@@ -644,6 +716,7 @@ function MemberView({route, navigation}) {
 }
 
 function InvitationView({route, navigation}) {
+  const { group } = route.params;
   const localStyles = {
     infoContainer: {
       backgroundColor: '#ffffff',
@@ -697,8 +770,9 @@ function InvitationView({route, navigation}) {
         <Text
           style={localStyles.infoText}
         >
-          Group name goes here
+          {group.name}
         </Text>
+        <Text>{"Invitor: " + group.uName}</Text>
       </View>
 
       <View style={{flexDirection:'row'}}>
@@ -718,6 +792,25 @@ function InvitationView({route, navigation}) {
 
         
       </View>
+    </View>
+  );
+}
+
+function AddGroupView({route, navigation}) {
+
+  const [text, setText] = useState('');
+  return (
+    <View>
+      <Text> Input IDs with spaces .... </Text>
+      <TextInput 
+        style={{height: 40, borderColor:'#000000', borderWidth: 2}}
+        placeholder="IDs..."
+        onChangeText={text => setText(text)}
+        defaultValue={text}      
+      />
+      
+      <Text> {text.split(' ').map(id=>id+'\n')} </Text>
+
     </View>
   );
 }
@@ -782,6 +875,17 @@ export default class Groups extends React.Component {
               title: 'Invitations'
             })}
           />
+
+          <Stack.Screen
+            name='create-group'
+            component={AddGroupView}
+            options={({route}) => ({
+              title: 'Create a new group'
+            })}
+
+          />
+
+          
 
         </Stack.Navigator>
       </View>
